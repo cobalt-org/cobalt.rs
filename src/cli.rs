26abad4051606bd2fe6e6ea374e7e4c22cec74ba
Benny Klotz <r3qnbenni@gmail.com>
2014-10-24 14:03:34 +0200

    basic Runner::run() implementation

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,13 @@
 pub struct Runner;
 
 impl Runner {
-    pub fn run() {
-        println!("Hello rustie");
+    pub fn run(path: &str) {
+        println!("Generating site in {}", path);
+
+        let documents = Runner::parse_documents(path.to_string() + "/_posts");
+    }
+
+    fn parse_documents(document_path: &str) {
+        println!("{}", document_path);
     }
 }
