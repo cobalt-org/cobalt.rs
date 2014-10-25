84392251f220d8b0426a52616d96307724b02c1f
Benny Klotz <r3qnbenni@gmail.com>
2014-10-25 16:29:22 +0200

    Implemented parsing of documents - parsed in Document has following members: name, user-set attributes & content

--- src/cli.rs
+++ src/cli.rs
@@ -9,7 +9,7 @@ pub struct Runner;
 
 impl Runner {
     pub fn run(path_string: &str) {
-        let base_path = os::make_absolute(&Path::new(path_string));
+        let base_path      = os::make_absolute(&Path::new(path_string));
         let documents_path = os::make_absolute(&Path::new((path_string.to_string() + "/_posts").as_slice()));
 
         println!("Generating site in {}", base_path.as_str().unwrap());
@@ -23,12 +23,12 @@ impl Runner {
 
         if paths.is_ok() {
             for path in paths.unwrap().iter() {
-                let attributes   = Runner::extract_attributes(path);
-                let content      = Runner::extract_content(path);
-
-                for attribute in attributes.iter() {
-                    println!("{}", attribute);
+                if path.extension_str().unwrap() != "tpl" {
+                    continue;
                 }
+
+                let attributes = Runner::extract_attributes(path);
+                let content    = Runner::extract_content(path);
 
                 documents.push(Document::new(attributes, content));
             }
@@ -36,25 +36,44 @@ impl Runner {
             println!("Path {} doesn't exist", documents_path.as_str().unwrap());
         }
 
+        /*for document in documents.iter() {
+            println!("{}", document.as_html());
+        }*/
+
         return documents;
     }
 
     fn extract_attributes(document_path: &Path) -> Vec<(String, String)> {
+        let mut attributes = vec!();
+        attributes.push(("name".to_string(), document_path.filestem_str().unwrap().to_string()));
+
         let content = File::open(document_path).read_to_string().unwrap();
 
-        // TODO: Regex matching for retrieving the attributes
+        let mut content_splits = content.as_slice().split_str("---");
 
-        vec![
-            ("Test Key".to_string(), "Test Value".to_string()),
-            ("Test Key2".to_string(), "Test Value2".to_string())
-        ]
+        let attribute_string = content_splits.nth(0u).unwrap();
+
+        for attribute_line in attribute_string.split_str("\n") {
+            if !attribute_line.contains_char(':') {
+                continue;
+            }
+
+            let mut attribute_split = attribute_line.split(':');
+
+            let key   = attribute_split.nth(0u).unwrap().trim_chars(' ').to_string();
+            let value = attribute_split.nth(0u).unwrap().trim_chars(' ').to_string();
+
+            attributes.push((key, value));
+        }
+
+        return attributes;
     }
 
     fn extract_content(document_path: &Path) -> String {
         let content = File::open(document_path).read_to_string().unwrap();
 
-        // TODO: Regex matching for retrieving the content
+        let mut content_splits = content.as_slice().split_str("---");
 
-        return "Test".to_string();
+        content_splits.nth(1u).unwrap().to_string()
     }
 }
