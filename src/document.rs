771a627d42f19c7f05b66bab0cf59d6ef208cdda
Benny Klotz <r3qnbenni@gmail.com>
2014-10-25 16:49:46 +0200

    implemented std::fmt::Show trait for Document, better for debugging purpose, started implementation for document html view

--- src/document.rs
+++ src/document.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 pub struct Document {
     attributes: Vec<(String, String)>,
     content: String,
@@ -9,5 +11,15 @@ impl Document {
             attributes: attributes,
             content: content,
         }
+    }
+
+    pub fn as_html(&self) -> &str {
+        "test"
+    }
+}
+
+impl fmt::Show for Document {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Attributes: {}\nContent: {}", self.attributes, self.content)
     }
 }
