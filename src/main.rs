80d6d583e3ec6e7e1e12fc48482d89b438c50011
Benny Klotz <r3qnbenni@gmail.com>
2014-10-28 23:01:14 +0100

    use getopts crate for cli

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,52 @@
 extern crate cobalt;
+extern crate getopts;
 
 use cobalt::Runner;
+use getopts::{optflag, getopts, OptGroup};
 use std::os;
 
-// TODO: implement cli with getopts
-// http://doc.rust-lang.org/getopts/
-fn main() {
-    let args        = os::args();
-    let use_default = args.len() == 1u;
+fn print_usage(program: &str, _opts: &[OptGroup]) {
+    println!("Usage: {} [path] [options]", program);
+    println!("-h --help\tUsage");
+    println!("-v --version\tDisplay version");
+}
 
-    if use_default {
-        Runner::run(".");
-    } else {
-        Runner::run(args[1].as_slice());
+fn print_version() {
+    let version = vec!["0", "0", "1"];
+
+    println!("{}", version.connect("."));
+}
+
+fn main() {
+    let args: Vec<String> = os::args();
+    let program = args[0].clone();
+
+    let opts = [
+        optflag("h", "help", "print this help menu"),
+        optflag("v", "version", "Display version")
+    ];
+
+    let matches = match getopts(args.tail(), opts) {
+        Ok(m) => { m }
+        Err(f) => { fail!(f.to_string()) }
+    };
+
+    if matches.opt_present("h") {
+        print_usage(program.as_slice(), opts);
+        return;
     }
+
+    if matches.opt_present("v") {
+        print_version();
+        return;
+    }
+
+    let input = if !matches.free.is_empty() {
+        matches.free[0].clone()
+    } else {
+        print_usage(program.as_slice(), opts);
+        return;
+    };
+
+    Runner::run(input.as_slice());
 }
