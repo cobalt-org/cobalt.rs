cobalt-org/cobalt.rs

commit 02f866cade00538c3415ab2bb7f37fde0c6289bf

fix for not being able to copy files inside a folder

src/cobalt.rs
@@ -46,9 +46,9 @@ pub fn build(config: &Config) -> Result<()> {
     let dest = dest.as_path();
 
     let template_extensions: Vec<&OsStr> = config.template_extensions
-                                                .iter()
-                                                .map(OsStr::new)
-                                                .collect();
+                                                 .iter()
+                                                 .map(OsStr::new)
+                                                 .collect();
 
     let layouts_path = source.join(&config.layouts);
     let posts_path = source.join(&config.posts);
@@ -144,6 +144,11 @@ pub fn build(config: &Config) -> Result<()> {
                 try!(fs::create_dir_all(&dest.join(relative)));
                 debug!("Created new directory {:?}", dest.join(relative));
             } else {
+                let parent_folder_path = dest.clone()
+                                             .join(Path::new(relative).parent().unwrap());
+
+                try!(fs::create_dir_all(&parent_folder_path));
+
                 try!(fs::copy(entry.path(), &dest.join(relative))
                          .map_err(|_| format!("Could not copy {:?}", entry.path())));
                 debug!("Copied {:?} to {:?}", entry.path(), dest.join(relative));

commit f8c60343429bef3d9fdc86036c82355a3fef7908

Improve dir creation during copy
This fixes two minor problems introduced by 02f866cade00538c3415ab2bb7f37fde0c6289bf:

- We disallow the use of unsafe `unwrap`, even if they were unlikely to
  blow up, because errors should not invoke a panic but be returned as a
  Result. In this case the unwrap is especially unsafe, since
  path.parent() returns None on paths like `/`.
- `dest.clone()` was called without need

src/cobalt.rs
@@ -141,10 +141,9 @@ pub fn build(config: &Config) -> Result<()> {
                 try!(fs::create_dir_all(&dest.join(relative)));
                 debug!("Created new directory {:?}", dest.join(relative));
             } else {
-                let parent_folder_path = dest.clone()
-                                             .join(Path::new(relative).parent().unwrap());
-
-                try!(fs::create_dir_all(&parent_folder_path));
+                if let Some(ref parent) = Path::new(relative).parent() {
+                    try!(fs::create_dir_all(&dest.join(parent)));
+                }
 
                 try!(fs::copy(entry.path(), &dest.join(relative))
                          .map_err(|_| format!("Could not copy {:?}", entry.path())));