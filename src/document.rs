e41fd3ae362b8ee020b8d3e4f339b3727718a60a
Lucio Franco <lucio.franco@du.edu>
2016-04-10 00:59:32 -0600

    Fix for Windows path issue (#83)

--- src/document.rs
+++ src/document.rs
@@ -58,7 +58,8 @@ impl Document {
     pub fn get_attributes(&self) -> HashMap<String, Value> {
         let mut data = HashMap::new();
         data.insert("name".to_owned(), Value::Str(self.name.clone()));
-        data.insert("path".to_owned(), Value::Str(self.path.clone()));
+        // We replace to swap back slashes to forward slashes to ensure the URL's are valid
+        data.insert("path".to_owned(), Value::Str(self.path.replace("\\", "/")));
         data.insert("is_post".to_owned(), Value::Bool(self.is_post));
 
         for key in self.attributes.keys() {
