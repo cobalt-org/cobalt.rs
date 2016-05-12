98a73e2bbd165dde016bc6bda42c1ed0dcb89b9b
Lucio Franco <lucio.franco@du.edu>
2016-05-12 10:16:20 -0600

    removed bootstrap from new command

--- src/new.rs
+++ src/new.rs
@@ -11,24 +11,13 @@ ignore:
 const default_liquid: &'static [u8] = b"<!DOCTYPE html>
 <html>
     <head>
-      <meta charset=\"utf-8\">
-    	<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
-    	<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+        <meta charset=\"utf-8\">
 
         {% if is_post %}
           <title>{{ title }}</title>
         {% else %}
        	  <title>Cobalt.rs Blog</title>
         {% endif %}
-
-	    <!-- Latest compiled and minified CSS -->
-     	<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css\" integrity=\"sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7\" crossorigin=\"anonymous\">
-      	<!-- Optional theme -->
-     	<!--<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css\" integrity=\"sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r\" crossorigin=\"anonymous\">-->
-
-      <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js\"></script>
-      	<!-- Latest compiled and minified JavaScript -->
-     	<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js\" integrity=\"sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS\" crossorigin=\"anonymous\"></script>
     </head>
     <body>
     <div>
@@ -72,7 +61,6 @@ const index_liquid: &'static [u8] = b"extends: default.liquid
         <h4>{{post.title}}</h4>
         <h4><a href=\"{{post.path}}\">{{ post.title }}</a></h4>
       </div>
-
     {% endfor %}
   </div>
 </div>
