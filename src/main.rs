89134be833ea2aa31bf1958f4398984e4a291628
Kracekumar R <me@kracekumar.com>
2016-11-20 14:26:22 -0500

    Fix linting as per cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -262,9 +262,11 @@ fn main() {
 
         "clean" => {
             let cwd = std::env::current_dir().unwrap_or(PathBuf::new());
-            let destdir = std::fs::canonicalize(PathBuf::from(&config.dest)).unwrap_or(PathBuf::new());
+            let destdir = std::fs::canonicalize(PathBuf::from(&config.dest))
+                .unwrap_or(PathBuf::new());
             if cwd == destdir {
-                warn!("Destination directory is same as current directory. The current directory will be deleted.")
+                warn!("Destination directory is same as current directory. The current directory \
+                       will be deleted.")
             }
             match fs::remove_dir_all(&config.dest) {
                 Ok(..) => info!("directory \"{}\" removed", &config.dest),
