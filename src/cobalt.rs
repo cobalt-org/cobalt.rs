77f89eb822a0d8c10332d543c069ed5486be458f
Uwe Arzt <mail@uwe-arzt.de>
2017-02-07 20:43:24 +0100

    Moved tests to end of file, use assert instead of assert_eq

--- src/cobalt.rs
+++ src/cobalt.rs
@@ -34,23 +34,6 @@ fn compare_paths(a: &Path, b: &Path) -> bool {
 }
 
 /// Checks if one path is the starting point of another path.
-/// The tests are taken from tests/fixtures/`posts_in_subfolder`/
-#[test]
-fn test_starts_with_path() {
-    let posts_folder = Path::new("tests/fixtures/posts_in_subfolder/posts");
-
-    assert_eq!(starts_with_path(Path::new("tests/fixtures/posts_in_subfolder"), posts_folder),
-               false);
-    assert_eq!(starts_with_path(Path::new("tests/fixtures/posts_in_subfolder/posts"),
-                                posts_folder),
-               true);
-    assert_eq!(starts_with_path(Path::new("tests/fixtures/posts_in_subfolder/posts/20170103"),
-                                posts_folder),
-               true);
-    assert_eq!(starts_with_path(Path::new("tests/fixtures/posts_in_subfolder/posts/2017/01/08"),
-                                posts_folder),
-               true);
-}
 fn starts_with_path(this: &Path, starts_with: &Path) -> bool {
     match (fs::canonicalize(this), fs::canonicalize(starts_with)) {
         (Ok(p), Ok(p2)) => p.starts_with(p2),
@@ -277,4 +260,18 @@ fn create_document_file<T: AsRef<Path>, R: AsRef<Path>>(content: &str,
     try!(file.write_all(&content.as_bytes()));
     info!("Created {}", file_path.display());
     Ok(())
+}
+
+// The tests are taken from tests/fixtures/`posts_in_subfolder`/
+#[test]
+fn test_starts_with_path() {
+    let posts_folder = Path::new("tests/fixtures/posts_in_subfolder/posts");
+
+    assert!(!starts_with_path(Path::new("tests/fixtures/posts_in_subfolder"), posts_folder));
+    assert!(starts_with_path(Path::new("tests/fixtures/posts_in_subfolder/posts"),
+                             posts_folder));
+    assert!(starts_with_path(Path::new("tests/fixtures/posts_in_subfolder/posts/20170103"),
+                             posts_folder));
+    assert!(starts_with_path(Path::new("tests/fixtures/posts_in_subfolder/posts/2017/01/08"),
+                             posts_folder));
 }
