2b5be84391288bb1045049a244d7339e2453dc4f
Ed Page <eopage@byu.net>
2017-05-24 17:29:04 -0500

    fix(CI): Protect against timing out
    Building rustfmt was slow enough that it sometimes timed out our CI, so
    telling travis to wait a little longer

--- ci/install.sh
+++ ci/install.sh
@@ -4,8 +4,9 @@ main() {
     curl https://sh.rustup.rs -sSf | \
         sh -s -- -y --default-toolchain $TRAVIS_RUST_VERSION
 
-    # Install rustfmt
-    cargo install rustfmt --force --vers 0.8.3
+    if [ "$TRAVIS_RUST_VERSION" = "stable" ]; then
+      travis_wait cargo install rustfmt --force --vers 0.8.3
+    fi
 
     if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
       cargo install clippy --force
