5d77b7fc1a08bf2994a599e852d0cf6462a7431d
Ed Page <eopage@byu.net>
2018-01-08 20:43:18 -0700

    feat(debug): Report asset files
    `cobalt debug files assets` will report all files found to be assets.
    This allows for quick sanity checking when debugging cobalt's processing
    and produces less noise if wanting to use `--trace` to see why the
    ignore patterns are doing what they do.

--- src/bin/cobalt/debug.rs
+++ src/bin/cobalt/debug.rs
@@ -1,6 +1,7 @@
 use clap;
 use cobalt;
 
+use args;
 use error::*;
 
 pub fn debug_command_args() -> clap::App<'static, 'static> {
@@ -10,6 +11,12 @@ pub fn debug_command_args() -> clap::App<'static, 'static> {
                         .about("Print syntax-highlight information")
                         .subcommand(clap::SubCommand::with_name("themes"))
                         .subcommand(clap::SubCommand::with_name("syntaxes")))
+        .subcommand(clap::SubCommand::with_name("files")
+                        .about("Print files associated with a collection")
+                        .args(&args::get_config_args())
+                        .arg(clap::Arg::with_name("COLLECTION")
+                                 .help("Collection name")
+                                 .index(1)))
 }
 
 pub fn debug_command(matches: &clap::ArgMatches) -> Result<()> {
@@ -27,6 +34,24 @@ pub fn debug_command(matches: &clap::ArgMatches) -> Result<()> {
                     }
                 }
                 _ => bail!(matches.usage()),
+            }
+        }
+        ("files", Some(matches)) => {
+            let config = args::get_config(matches)?;
+            let config = config.build()?;
+            let collection = matches.value_of("COLLECTION");
+            match collection {
+                Some("assets") => {
+                    for file_path in config.assets.files() {
+                        println!("{:?}", file_path);
+                    }
+                }
+                None => {
+                    bail!("Must specify collection");
+                }
+                _ => {
+                    bail!("Collection is not yet supported");
+                }
             }
         }
         _ => bail!(matches.usage()),
