29d5a74e4438744332f2719de7b6ae18578c16e0
Ed Page <eopage@byu.net>
2018-03-03 11:47:41 -0700

    feat(debug): Print pages and posts files

--- src/bin/cobalt/debug.rs
+++ src/bin/cobalt/debug.rs
@@ -53,6 +53,28 @@ pub fn debug_command(matches: &clap::ArgMatches) -> Result<()> {
                         println!("{:?}", file_path);
                     }
                 }
+                Some("pages") => {
+                    let pages = config.pages.build()?;
+                    for file_path in pages.pages.files() {
+                        println!("{:?}", file_path);
+                    }
+                    if let Some(ref drafts) = pages.drafts {
+                        for file_path in drafts.files() {
+                            println!("{:?}", file_path);
+                        }
+                    }
+                }
+                Some("posts") => {
+                    let posts = config.posts.build()?;
+                    for file_path in posts.pages.files() {
+                        println!("{:?}", file_path);
+                    }
+                    if let Some(ref drafts) = posts.drafts {
+                        for file_path in drafts.files() {
+                            println!("{:?}", file_path);
+                        }
+                    }
+                }
                 None => {
                     bail!("Must specify collection");
                 }
