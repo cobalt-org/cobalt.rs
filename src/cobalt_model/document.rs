50d03ac982fbc7721fca46cb1e77d1de426b21b4
Ethan Frei <ethan.frei@gmail.com>
2018-10-12 21:18:32 -0500

    feat: allow divider above front matter

--- src/cobalt_model/document.rs
+++ src/cobalt_model/document.rs
@@ -46,14 +46,33 @@ impl<T: frontmatter::Front> fmt::Display for DocumentBuilder<T> {
 fn split_document(content: &str) -> Result<(Option<&str>, &str)> {
     lazy_static! {
         static ref FRONT_MATTER_DIVIDE: regex::Regex = regex::Regex::new(r"---\s*\r?\n").unwrap();
-        static ref FRONT_MATTER: regex::Regex = regex::Regex::new(r"\A---\s*\r?\n[\s\S]*---\s*\r?\n").unwrap();
+        static ref FRONT_MATTER: regex::Regex = regex::Regex::new(r"\A---\s*\r?\n([\s\S]*\n)?---\s*\r?\n").unwrap();
     }
 
-    // remove divide at beginning of file if exists
-    let content = if FRONT_MATTER.is_match(content) {
-        content.splitn(2, '\n').skip(1).next().unwrap()
-    } else { content };
+    if FRONT_MATTER.is_match(content) {
+        // skip first empty string
+        let mut splits = FRONT_MATTER_DIVIDE.splitn(content, 3).skip(1);
 
+        // split between dividers
+        let front_split = splits.next().unwrap_or("");
+
+        // split after second divider
+        let content_split = splits.next().unwrap_or("");
+
+        if front_split.is_empty() {
+            Ok((None, content_split))
+        } else {
+            Ok((Some(front_split), content_split))
+        }
+    } else {
+        deprecated_split_front_matter(content)
+    }
+}
+
+fn deprecated_split_front_matter(content: &str) -> Result<(Option<&str>, &str)> {
+    lazy_static! {
+        static ref FRONT_MATTER_DIVIDE: regex::Regex = regex::Regex::new(r"(\A|\n)---\s*\r?\n").unwrap();
+    }
     if FRONT_MATTER_DIVIDE.is_match(content) {
         let mut splits = FRONT_MATTER_DIVIDE.splitn(content, 2);
 
@@ -71,8 +90,8 @@ fn split_document(content: &str) -> Result<(Option<&str>, &str)> {
     } else {
         Ok((None, content))
     }
-
 }
+
 
 #[cfg(test)]
 mod test {
@@ -114,7 +133,7 @@ mod test {
     fn split_document_deprecated_empty_body() {
         let input = "cobalt_model\n---\n";
         let (cobalt_model, content) = split_document(input).unwrap();
-        assert_eq!(cobalt_model.unwrap(), "cobalt_model\n");
+        assert_eq!(cobalt_model.unwrap(), "cobalt_model");
         assert_eq!(content, "");
     }
 
@@ -132,6 +151,15 @@ mod test {
         let (cobalt_model, content) = split_document(input).unwrap();
         assert_eq!(cobalt_model.unwrap(), "cobalt_model\n");
         assert_eq!(content, "body");
+    }
+
+    #[test]
+    fn split_document_no_new_line_after_front_matter() {
+        let input = "invalid_front_matter---\nbody";
+        let (cobalt_model, content) = split_document(input).unwrap();
+        assert!(cobalt_model.is_none());
+        assert_eq!(content, input);
+
     }
 
     #[test]
