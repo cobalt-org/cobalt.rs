ad62b2fddec662d202cd59ad702650a62935f9dd
Ethan Frei <ethan.frei@gmail.com>
2018-10-30 22:05:02 -0500

    feat: Jekyll-style frontmatter divider
    This change adds support for Jekyll's frontmatter divider style (`---` before and after) which also aligns with yaml's document separators.
    
    The old style of dividers is being kept around but marked as deprecated.
    
    Fixes #431 
    
    BREAKING CHANGE: documents with empty frontmatter followed by `---` will no longer build

--- src/cobalt_model/document.rs
+++ src/cobalt_model/document.rs
@@ -38,7 +38,7 @@ impl<T: frontmatter::Front> fmt::Display for DocumentBuilder<T> {
         if front.trim().is_empty() {
             write!(f, "{}", self.content)
         } else {
-            write!(f, "{}\n---\n{}", front, self.content)
+            write!(f, "---\n{}\n---\n{}", front, self.content)
         }
     }
 }
@@ -46,9 +46,38 @@ impl<T: frontmatter::Front> fmt::Display for DocumentBuilder<T> {
 fn split_document(content: &str) -> Result<(Option<&str>, &str)> {
     lazy_static! {
         static ref FRONT_MATTER_DIVIDE: regex::Regex = regex::Regex::new(r"---\s*\r?\n").unwrap();
+        static ref FRONT_MATTER: regex::Regex =
+            regex::Regex::new(r"\A---\s*\r?\n([\s\S]*\n)?---\s*\r?\n").unwrap();
     }
 
+    if FRONT_MATTER.is_match(content) {
+        // skip first empty string
+        let mut splits = FRONT_MATTER_DIVIDE.splitn(content, 3).skip(1);
+
+        // split between dividers
+        let front_split = splits.next().unwrap_or("");
+
+        // split after second divider
+        let content_split = splits.next().unwrap_or("");
+
+        if front_split.is_empty() {
+            Ok((None, content_split))
+        } else {
+            Ok((Some(front_split), content_split))
+        }
+    } else {
+        deprecated_split_front_matter(content)
+    }
+}
+
+fn deprecated_split_front_matter(content: &str) -> Result<(Option<&str>, &str)> {
+    lazy_static! {
+        static ref FRONT_MATTER_DIVIDE: regex::Regex =
+            regex::Regex::new(r"(\A|\n)---\s*\r?\n").unwrap();
+    }
     if FRONT_MATTER_DIVIDE.is_match(content) {
+        warn!("Trailing separators are deprecated. We recommend frontmatters be surrounded, above and below, with ---");
+
         let mut splits = FRONT_MATTER_DIVIDE.splitn(content, 2);
 
         // above the split are the attributes
@@ -88,7 +117,7 @@ mod test {
     }
 
     #[test]
-    fn split_document_empty_front_matter() {
+    fn split_document_deprecated_empty_front_matter() {
         let input = "---\nBody";
         let (cobalt_model, content) = split_document(input).unwrap();
         assert!(cobalt_model.is_none());
@@ -96,11 +125,43 @@ mod test {
     }
 
     #[test]
-    fn split_document_empty_body() {
-        let input = "cobalt_model---\n";
+    fn split_document_empty_front_matter() {
+        let input = "---\n---\nBody";
+        let (cobalt_model, content) = split_document(input).unwrap();
+        assert!(cobalt_model.is_none());
+        assert_eq!(content, "Body");
+    }
+
+    #[test]
+    fn split_document_deprecated_empty_body() {
+        let input = "cobalt_model\n---\n";
         let (cobalt_model, content) = split_document(input).unwrap();
         assert_eq!(cobalt_model.unwrap(), "cobalt_model");
         assert_eq!(content, "");
+    }
+
+    #[test]
+    fn split_document_empty_body() {
+        let input = "---\ncobalt_model\n---\n";
+        let (cobalt_model, content) = split_document(input).unwrap();
+        assert_eq!(cobalt_model.unwrap(), "cobalt_model\n");
+        assert_eq!(content, "");
+    }
+
+    #[test]
+    fn split_document_front_matter_and_body() {
+        let input = "---\ncobalt_model\n---\nbody";
+        let (cobalt_model, content) = split_document(input).unwrap();
+        assert_eq!(cobalt_model.unwrap(), "cobalt_model\n");
+        assert_eq!(content, "body");
+    }
+
+    #[test]
+    fn split_document_no_new_line_after_front_matter() {
+        let input = "invalid_front_matter---\nbody";
+        let (cobalt_model, content) = split_document(input).unwrap();
+        assert!(cobalt_model.is_none());
+        assert_eq!(content, input);
     }
 
     #[test]
