6ae79c19ed547ba0bab890606d981f01a0d7af5f
Andreas Rottmann <a.rottmann@gmx.at>
2020-02-12 21:50:32 +0100

    chore: Some `as_deref()` simplifications
    These were identified by current beta clippy (4ee1206 2020-02-01).

--- src/cobalt.rs
+++ src/cobalt.rs
@@ -397,11 +397,7 @@ fn create_rss(
     debug!("Creating RSS file at {}", rss_path.display());
 
     let title = &collection.title;
-    let description = collection
-        .description
-        .as_ref()
-        .map(|s| s.as_str())
-        .unwrap_or("");
+    let description = collection.description.as_deref().unwrap_or("");
     let link = collection
         .base_url
         .as_ref()
@@ -446,11 +442,7 @@ fn create_jsonfeed(
     debug!("Creating jsonfeed file at {}", jsonfeed_path.display());
 
     let title = &collection.title;
-    let description = collection
-        .description
-        .as_ref()
-        .map(|s| s.as_str())
-        .unwrap_or("");
+    let description = collection.description.as_deref().unwrap_or("");
     let link = collection
         .base_url
         .as_ref()
