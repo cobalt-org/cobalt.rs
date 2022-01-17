3d44328314890e4e88315c91b582297bb73b2d6f
Ed Page <eopage@gmail.com>
2022-01-17 13:27:41 -0600

    test: Increase CLI test coverage under different feature sets

--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -1,5 +1,10 @@
 #[test]
 #[cfg(feature = "syntax-highlight")]
 fn cli_tests() {
-    trycmd::TestCases::new().case("tests/cmd/*.md");
+    let t = trycmd::TestCases::new();
+    t.case("tests/cmd/*.md");
+    #[cfg(not(feature = "serve"))]
+    {
+        t.skip("tests/cmd/errors.md");
+    }
 }
