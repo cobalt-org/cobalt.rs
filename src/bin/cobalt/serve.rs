a114ebe7e23dff7d0aaaed6642bfa2f042fedfd8
Jacob Hummer <jcbhmr@outlook.com>
2024-02-27 15:34:36 -0600

    Update serve.rs

--- src/bin/cobalt/serve.rs
+++ src/bin/cobalt/serve.rs
@@ -46,7 +46,7 @@ impl ServeArgs {
         let server = server.build();
 
         let mut config = self.config.load_config()?;
-        debug!("Overriding config `site.base_url` with `{}`", server.addr());
+        debug!("Overriding config `site.base_url` with `/`");
         config.site.base_url = Some("/".into());
         let mut config = cobalt::cobalt_model::Config::from_config(config)?;
         debug!(
